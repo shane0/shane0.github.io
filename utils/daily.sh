a6e85c49156c57396421a845eebd3eb6f64c91ea
shane0 <shane.null@gmail.com>
2023-05-24 22:32:33 -0700

    'moon theme on slide'

--- utils/daily.sh
+++ utils/daily.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-pandoc -t revealjs -s -o ./slides/daily.html ./slides/daily.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ -V theme=solarized
+# pandoc -t revealjs -s -o ./slides/daily.html ./slides/daily.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ -V theme=solarized
+pandoc -t revealjs -s -o ./slides/daily.html ./slides/daily.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ -V theme=moon
 cp ./slides/daily.html ./docs/
